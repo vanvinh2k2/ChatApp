79c8c3719aea4df5d714ce13781cc26cca31b44a
vinhngo24 <vinhngo240302@gmail.com>
2023-04-28 17:02:53 +0700

    commit js

--- mychatapp/urls.py
+++ mychatapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from .views import index, detail
+from .views import index, detail, sent_chat
 
 
 urlpatterns = [
     path('', index, name="index"),
     path('friends/<str:pk>/', detail, name="detail"),
+    path('send_msg/<str:pk>/', sent_chat, name="send_msg"),
 ]
--- mychatapp/views.py
+++ mychatapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import ChatMessageForm
+from django.core.serializers import serialize
 from .models import *
 # Create your views here.
 
@@ -16,13 +17,23 @@ def detail(request, pk):
     user = request.user.frofile
     profile = Frofile.objects.get(id=friend.profile.id)
     chat = ChatMessage.objects.all()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ChatMessageForm(request.POST)
         if form.is_valid():
             chat_message = form.save(commit=False)
             chat_message.msg_sender = user
             chat_message.msg_receiver = profile
             chat_message.save()
+
     context = {'friend': friend, 'form': form, 'chats': chat, 'user': user, 'profile': profile}
     return render(request, 'mychatapp/detail.html', context)
+
+def sent_chat(request, pk):
+    new_chat = request.POST.get('msg')
+    friend = Friend.objects.get(profile_id=pk)
+    user = request.user.frofile
+    profile = Frofile.objects.get(id=friend.profile.id)
+    new_chat_message = ChatMessage.objects.create(body=new_chat, msg_sender=user, msg_receiver=profile, seen = False)
+    serialized_data = serialize('json', [new_chat_message])
+    return JsonResponse({"result": new_chat}, safe=False)
 
